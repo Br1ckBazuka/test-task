d034e324428c7e1108d143d0dc40a75589134974
Br1ckBazuka <br1ckbazuka2307@gmail.com>
2025-07-27 21:39:01 +0500

    task 1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿class MaxLength
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Вставьте строку из .txt файла: ");
+            var line = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                Console.WriteLine("Введенная строка пустая");
+                continue;
+            }
+            string[] words = line.Split();
+            var validWords = words
+                .Where(word =>
+                    word.Any(char.IsLetter)
+                    && word.All(c => char.IsLetter(c) || c == '-' || c == '\'')
+                )
+                .ToArray();
+
+            if (validWords.Length == 0)
+            {
+                Console.WriteLine("Строка содержит только цифры или символы");
+                continue;
+            }
+
+            if (words.Length != validWords.Length)
+            {
+                Console.WriteLine("Строка содержит цифры или символы");
+            }
+            // добавить проверку если самых длинных слов несколько
+            var maxLength = validWords.Max(word => word.Length);
+            var longWords = validWords.Where(word => word.Length == maxLength).ToArray();
+
+            Console.WriteLine(
+                longWords.Length == 1
+                    ? $"Слово с максимальной длиной: {longWords[0]}"
+                    : $"Слова с максимальной длиной ({maxLength}): {string.Join(",", longWords)}"
+            );
+        }
+    }
+}
