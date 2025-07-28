05ce183fc2d688f7b07ede170a05b532036e08dd
Br1ckBazuka <br1ckbazuka2307@gmail.com>
2025-07-28 08:57:44 +0500

    update Task1

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -2,44 +2,30 @@
 {
     static void Main()
     {
-            Console.WriteLine("Вставьте строку из .txt файла: ");
-            var line = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(line))
-            {
-                Console.WriteLine("Введенная строка пустая");
-                return;
-            }
-            string[] words = line.Split();
-            var hasSymbolsOrNumber = words.Any(word => word.Any(c=> !char.IsLetter(c)));
-            if (hasSymbolsOrNumber)
-            {
-                Console.WriteLine("Строка содержит символы или цифры");
-                return;
-            }
-            var longWord = words.OrderByDescending(word => word.Length).First();
-            Console.WriteLine($"Самое длинное слово: {longWord}");
+        var filePath = "../../../test.txt";
+        var line = File.ReadAllText(filePath);
 
-                            //Обработка случаев для слов с " - " и " ' "
-            // var validWords = words
-            //     .Where(word =>
-            //         word.Any(char.IsLetter)
-            //         && word.All(c => char.IsLetter(c) || c == '-' || c == '\'')
-            //     )
-            //     .ToArray();
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            Console.WriteLine("Строка пустая");
+            return;
+        }
 
-            // if (validWords.Length == 0)
-            // {
-            //     Console.WriteLine("Строка содержит только цифры или символы");
-            //     return;
-            // }
-            
-                            //Обработка случаев если самых длинных слов несколько
-            // var maxLength = words.Max(word => word.Length);
-            // var longWords = words.Where(word => word.Length == maxLength).ToArray();
-            // Console.WriteLine(
-            //     longWords.Length == 1
-            //         ? $"Слово с максимальной длиной: {longWords[0]}"
-            //         : $"Слова с максимальной длиной ({maxLength}): {string.Join(",", longWords)}"
-            // );
+        string[] words = line.Split(
+            ' ',
+            StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries
+        );
+
+        var letter = words.Where(word => word.All(char.IsLetter)).ToArray();
+
+        if (letter.Length == 0)
+        {
+            Console.WriteLine("Строка содержит только символы и цифры");
+            return;
+        }
+
+        var longWord = letter.MaxBy(word => word.Length);
+        Console.WriteLine($"Самое длинное слово: {longWord}");
     }
 }
+
